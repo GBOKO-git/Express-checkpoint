5fc539b1786d5f0e0b1c88cce4b1fe79a5253a14
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-04-13 11:50:33 +0000

    ajout de middleware pour s'assurer que le client à renseigner tout les champ

--- src/script/main.js
+++ src/script/main.js
@@ -5,7 +5,15 @@ const sendBtn = document.getElementById("sendBtn");
 sendBtn.addEventListener("click", () => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
-  alert(
-    `Bienvenue Mr/Mme ${name}, merci de nous avoir contacter! \n Nous vous reviendrons bientot sur votre ${email}!`
-  );
+  const message = document.getElementById("message").value;
+
+
+  if (!name || !email || !message) {
+    alert("Vueillez reseigner tou les champ!")
+  } else {
+    alert(
+      `Bienvenue Mr/Mme ${name}, merci de nous avoir contacter! \n Nous vous reviendrons bientot sur votre ${email}!`
+    );
+  }
+  
 });
